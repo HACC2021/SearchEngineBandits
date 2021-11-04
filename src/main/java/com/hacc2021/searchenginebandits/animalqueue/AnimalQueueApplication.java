91f978c12566f27e3089ae71eb02952155f7ab10
Linus Ehlers - leh <Linus.Ehlers@ppi.de>
2021-11-04 12:16:59 -1000

    Initial commit spring boot

---
+++ src/main/java/com/hacc2021/searchenginebandits/animalqueue/AnimalQueueApplication.java
@@ -0,0 +1,12 @@
+package com.hacc2021.searchenginebandits.animalqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalQueueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hacc2021/searchenginebandits/animalqueue/AnimalQueueApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hacc2021.searchenginebandits.animalqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalQueueApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
