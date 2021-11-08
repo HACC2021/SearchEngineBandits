62de892e585ae8ef0fbfe51c4a58223feb428177
Linus Ehlers - leh <Linus.Ehlers@ppi.de>
2021-11-08 12:09:15 -1000

    Optimized error handling

--- src/main/java/com/hacc2021/searchenginebandits/animalqueue/controller/ErrorController.java
+++ src/main/java/com/hacc2021/searchenginebandits/animalqueue/controller/ErrorController.java
@@ -1,23 +1,29 @@
 package com.hacc2021.searchenginebandits.animalqueue.controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.hacc2021.searchenginebandits.animalqueue.service.OwnerService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @EnableAutoConfiguration
 @RequiredArgsConstructor
-public class ErrorController {
+@Slf4j
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-    @GetMapping("/error")
-    public String handleError(final HttpServletRequest request, final Model model) {
-        request.setAttribute("statusCode", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
-        return "error";
+    @RequestMapping("/error")
+    public ModelAndView handleError(final HttpServletRequest request) {
+        final Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        log.error("Catched error {}", statusCode);
+
+        final ModelAndView mav = new ModelAndView("error");
+        mav.addObject("statusCode", statusCode);
+        mav.setViewName("error");
+        return mav;
     }
 }
