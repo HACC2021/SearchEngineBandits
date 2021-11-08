f5aac686a6dded74710b232282295b0212f5c85e
Linus Ehlers - leh <Linus.Ehlers@ppi.de>
2021-11-07 20:50:28 -1000

    Added sample methods to WebController and changed java spring config

--- src/main/java/com/hacc2021/searchenginebandits/animalqueue/Config.java
+++ src/main/java/com/hacc2021/searchenginebandits/animalqueue/Config.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories
 public class Config {
     @Bean
     @Description("Spring Message Resolver")
     public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
         return messageSource;
     }
