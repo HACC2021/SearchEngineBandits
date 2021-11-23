cb622a68e76b6954f46c0182365bfa9dc14f8c42
Linus Ehlers - leh <Linus.Ehlers@ppi.de>
2021-11-23 10:00:19 -1000

    Fixed email sending

--- src/main/java/com/hacc2021/searchenginebandits/animalqueue/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/hacc2021/searchenginebandits/animalqueue/service/impl/NotificationServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hacc2021.searchenginebandits.animalqueue.service.impl;
 
+import com.hacc2021.searchenginebandits.animalqueue.model.Owner;
 import com.hacc2021.searchenginebandits.animalqueue.service.api.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -19,16 +22,21 @@ public class NotificationServiceImpl implements NotificationService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmailAddress;
+
     @Override
-    public void sendNotification(final String toEmail, final String subject, final String htmlMessage) {
+    public void sendNotification(final Owner owner, final String subject, final String htmlMessage) {
         try {
             final MimeMessage mimeMessage = mailSender.createMimeMessage();
-            final InternetAddress to = new InternetAddress(toEmail);
+            final InternetAddress to = new InternetAddress(owner.getEmailAddress(), owner.getName());
+            final InternetAddress from = new InternetAddress(fromEmailAddress, "HDOA Pawsome Pickup");
             mimeMessage.addRecipient(Message.RecipientType.TO, to);
+            mimeMessage.setFrom(from);
             mimeMessage.setSubject(subject);
             mimeMessage.setContent(htmlMessage, "text/html");
             mailSender.send(mimeMessage);
-        } catch (final MessagingException e) {
+        } catch (final MessagingException | UnsupportedEncodingException e) {
             log.error("Failed to send email message.", e);
         }
     }
